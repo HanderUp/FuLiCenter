fadae9ec844d377c8c3e58eea16ec9bdfd581aa7
张伟尧 <june929546724@Gmail.com>
2016-10-24 12:36:45 +0800

    修改FuliCenterAppliaction中的BUG

--- app/src/main/java/cn/ucai/fulicenter/FuLiCenterApplication.java
+++ app/src/main/java/cn/ucai/fulicenter/FuLiCenterApplication.java
@@ -7,10 +7,14 @@ import android.app.Application;
  */
 
 public class FuLiCenterApplication extends Application {
+    public static FuLiCenterApplication application;
     private static FuLiCenterApplication instance;
 
-    public FuLiCenterApplication() {
-        instance = this;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        application=this;
+        instance=this;
     }
 
     public static FuLiCenterApplication getInstance() {
