6f1d71977429234e2a389635c93705a55a7e7d31
张伟尧 <june929546724@Gmail.com>
2016-11-03 23:06:34 +0800

    添加数据库操作的三个工具类

---
+++ app/src/main/java/cn/ucai/fulicenter/dao/DBOpenHelper.java
@@ -0,0 +1,54 @@
+package cn.ucai.fulicenter.dao;
+
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import cn.ucai.fulicenter.I;
+
+public class DBOpenHelper extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION = 1;
+    private static DBOpenHelper intance;
+    private static final String FULICENTER_USER_TABLE_CREAT = "CREATE TABLE "
+            + UserDao.USER_TABLE_NAME + " ("
+            + UserDao.USER_COLUMN_NAME + " TEXT PRIMARY KEY, "
+            + UserDao.USER_COLUMN_NICK + " TEXT, "
+            + UserDao.USER_COLUMN_AVATAR_ID + " INTEGER, "
+            + UserDao.USER_COLUMN_AVATAR_TYPE + " INTEGER, "
+            + UserDao.USER_COLUMN_AVATAR_PATH + " TEXT, "
+            + UserDao.USER_COLUMN_AVATAR_SUFFIX + " TEXT, "
+            + UserDao.USER_COLUMN_AVATAR_LASTUPDATE_TIME + " TEXT):";
+    public static DBOpenHelper getIntance(Context context) {
+        if (intance == null) {
+            intance = new DBOpenHelper(context);
+        }
+        return intance;
+    }
+
+    public DBOpenHelper(Context context) {
+        super(context, getUserDatabaseName(), null, DATABASE_VERSION);
+    }
+
+    private static String getUserDatabaseName() {
+        return I.User.TABLE_NAME + "_demo.db";
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(FULICENTER_USER_TABLE_CREAT);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+
+    public void closeDB() {
+        if (intance != null) {
+            SQLiteDatabase db = intance.getWritableDatabase();
+            db.close();
+            intance = null;
+        }
+    }
+}
