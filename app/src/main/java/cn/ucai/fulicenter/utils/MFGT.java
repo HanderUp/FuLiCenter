e1d063e8c3339c14464056f17bd69d6ef76a0200
张伟尧 <june929546724@Gmail.com>
2016-11-04 17:28:49 +0800

    给个人资料填充数据，并添加退出登录的方法

--- app/src/main/java/cn/ucai/fulicenter/utils/MFGT.java
+++ app/src/main/java/cn/ucai/fulicenter/utils/MFGT.java
@@ -14,6 +14,7 @@ import cn.ucai.fulicenter.activity.GoodsDetailsActivity;
 import cn.ucai.fulicenter.activity.LoginActivity;
 import cn.ucai.fulicenter.activity.MainActivity;
 import cn.ucai.fulicenter.activity.RegisterActivity;
+import cn.ucai.fulicenter.activity.UserProfileActivity;
 import cn.ucai.fulicenter.bean.BoutiqueBean;
 import cn.ucai.fulicenter.bean.CategoryChildBean;
 
@@ -75,5 +76,9 @@ public class MFGT {
     public static void startActivityForResult(Activity context, Intent intent, int requestCode) {
         context.startActivityForResult(intent, requestCode);
         context.overridePendingTransition(R.anim.push_right_in,R.anim.push_right_out);
+    }
+
+    public static void gotoSettings(Activity context) {
+        startActivity(context, UserProfileActivity.class);
     }
 }
