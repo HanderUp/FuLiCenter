cf7ab1b8e177077ce503561fb334d27d064f1b55
张伟尧 <june929546724@Gmail.com>
2016-11-13 14:09:41 +0800

    添加购物车Adapter及布局文件

---
+++ app/src/main/java/cn/ucai/fulicenter/adapter/CartAdapter.java
@@ -0,0 +1,88 @@
+package cn.ucai.fulicenter.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.support.v7.widget.RecyclerView.Adapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.bean.CartBean;
+import cn.ucai.fulicenter.utils.MFGT;
+
+/**
+ * Created by Administrator on 2016/10/24.
+ */
+
+public class CartAdapter extends Adapter<CartAdapter.CartViewHolder> {
+    Context mContext;
+    ArrayList<CartBean> mList;
+
+    public CartAdapter(Context context, ArrayList<CartBean> list) {
+        mContext = context;
+        mList = new ArrayList<>();
+        mList.addAll(list);
+    }
+
+    @Override
+    public CartViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        CartViewHolder holder = new CartViewHolder(LayoutInflater.from(mContext)
+                .inflate(R.layout.item_cart, parent, false));
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(CartViewHolder holder, int position) {
+        CartBean cartBean = mList.get(position);
+//        ImageLoader.downloadImg(mContext, holder.mIvBoutiqueImg, boutiqueBean.getImageurl());
+//        holder.mTvBoutiqueTitle.setText(boutiqueBean.getTitle());
+//        holder.mTvBoutiqueName.setText(boutiqueBean.getName());
+//        holder.mTvBoutiqueDescription.setText(boutiqueBean.getDescription());
+//        holder.mLayoutBoutiqueItem.setTag(boutiqueBean);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mList != null ? mList.size() : 0;
+    }
+
+    public void initData(ArrayList<CartBean> list) {
+        if (mList != null) {
+            mList.clear();
+        }
+        mList.addAll(list);
+        notifyDataSetChanged();
+    }
+
+    class CartViewHolder extends ViewHolder{
+        @BindView(R.id.cb_cart_selected)
+        CheckBox cbCartSelected;
+        @BindView(R.id.iv_cart_thumb)
+        ImageView ivCartThumb;
+        @BindView(R.id.tv_cart_good_name)
+        TextView tvCartGoodName;
+        @BindView(R.id.iv_cart_add)
+        ImageView ivCartAdd;
+        @BindView(R.id.iv_cart_count)
+        TextView ivCartCount;
+        @BindView(R.id.iv_cart_del)
+        ImageView ivCartDel;
+        @BindView(R.id.tv_cart_price)
+        TextView tvCartPrice;
+
+        CartViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+    }
+}
